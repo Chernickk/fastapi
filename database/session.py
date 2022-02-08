43d0605aa9c1c745f3afaf76e0283ff1f5d861bb
nik <17rolex@gmail.com>
2022-02-08 16:49:06 +0300

    init

---
+++ database/session.py
@@ -0,0 +1,9 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from core.config import settings
+
+engine = create_async_engine(
+    settings.SQLALCHEMY_DATABASE_URI
+)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
